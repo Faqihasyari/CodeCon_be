ab4e9631043b1d4071d365af4e9164f8860f3103
Faqihasyari <faqih8158@gmail.com>
2025-01-12 21:21:20 +0700

    capek serius

--- lib/airtable/settings.dart
+++ lib/airtable/settings.dart
@@ -1,12 +1,15 @@
+import 'dart:io';
+
 import 'package:code_con_course_be/airtable/airtable_data.dart';
 import 'package:code_con_course_be/duitku/entities/duitku_merchant_data.dart';
 
 final airtableData = AirtableData(
   baseId: 'appS0dZg9V7UrR0pD',
   tableId: 'tbl5bZ2DNO16e2t1K',
-  token:
-      'patK2pjtwhKA6jI2D.b32b57fd7abf466615833d40eb40c3c18fdf374a5da0b0d980edcef378d10106',
+  token: Platform.environment['AIRTABLE_TOKEN']!,
 );
 
-final mechantData =
-    DuitkuMerchantData(merchantCode: 'DS21228', apiKey: '11e5464a78ff9bd043fedae42a1fcc33');
+final mechantData = DuitkuMerchantData(
+  merchantCode: 'DS21228',
+  apiKey: Platform.environment['DUITKU_API_KEY']!,
+);
